6c45e789599e100afbdef95e6cffb99dc36ab0a4
klebin157 <kleberrcps.ks@gmail.com>
2018-08-24 20:28:22 -0300

    alteração codigo login

--- conecta.php
+++
@@ -1,7 +0,0 @@
-<?php
-$localhost = "localhost";
-$usuario = "root";
-$senha = "";
-$database = "teste";
-
-$link = mysqli_connect($localhost , $usuario , $senha , $database) or die("Não foi possivel realizar conexão com o banco");
--- login.php
+++ login.php
@@ -3,39 +3,63 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
         <title></title>
     </head>
     <body>
-        <h1>Login</h1>
+           <p class="text-center"</p>
+        <!--- Logo -->
+    <div class="logo">
+    <img src="img/logo/Logo%20final2.png" alt="">
+    </div>
+       
         <form action="validarlogin.php" method="POST">
-            <input type="text" name="login" placeholder="Login">
-            <input type="password" name="senha" placeholder="Senha">
-            <input type="submit" value="Acessar">     
+               <p class="text-center"</p>
+           <div class="largura-site">  
+<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Contato</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Loja</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Login</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Cadastrar</a>
+  </li>
+</ul>
+        <h1>Login</h1>
+        <h2>CADASTRAR</h2>
+            <input type="text" name="login" placeholder="Login"><br><br>
+            <input type="password" name="senha" placeholder="Senha"><br><br>
+            <input type="submit" value="Acessar"> 
+            <div>
+                <a class="dropdown-item" href="#">Esqueceu sua senha?</a><br><br>
+            </div>
+            
+            <div
+             
+                <p>
+                    Porque se cadastrar no nosso site? Tenha mais facilidade
+                    em executar seus pedidos e nos enviar a decoração que desejar
+                    em nossos produtos. Nós da juana personalizaçôes buscamos sempre
+                    o maior conforto e melhor atendimento aos nossos clientes.
+                    Junte-se a nós e se CADASTRE logo abaixo.
+                </p>
+                <a href="cadastro.html">Clique AQUI e faça seu cadastro.</a>
+                <p>
+                    Caso não deseje se cadastrar ,<a href="contato.html">Clique AQUI</a> para que possa
+                    vizualizar nossos meios de contatos.
+                </p>
+ 
         </form>       
-            <?php
-            include_once 'conecta .php';
-            
-            $query = "select * from produto";
-            $result = mysqli_query($link, $query);
-            
-            if(mysqli_num_rows($result) > 0){
-                $linha = mysqli_fetch_array($result);
-                
-        
-            ?>
-        <table>
-                 <?php 
-                    while (mysql_fetch_array($result)) {
-                             
-                 ?>  
-            <tr> 
-            <td><img src="<?php echo $linha ["imagem"]?>"/></td>
-            <td><?php echo $linha ["descricao"]?></td>
-            <td><?php echo $linha ["valor"]?></td>
-            </tr>
-        </table>
-        
-
-              
-    </body>
-</html>
+          
